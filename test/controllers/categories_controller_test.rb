c91ed722f753cfd2eb6c0f993cb1642bf3915b57
Yasmin <yasmin312002@hotmail.com>
2019-10-23 10:56:01 -0700

    added tests to categories controller

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 describe CategoriesController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index action" do
+
+    it "gives back a successful response" do
+      get categories_path
+      must_respond_with :success
+    end
+
+    it "gives back a 404 if there are no works available" do
+      get categories_path
+    
+    end
+  end
+
+  describe 'show action' do
+    it 'responds with a success when id given exists' do
+      valid_work = Category.first
+      
+      get category_path(valid_category.id)
+
+      must_respond_with :success
+
+    end
+  end
+
+   describe 'show action' do
+
+    it 'responds with a success when id given exists' do
+      valid_category = Category.first
+      
+      get category_path(valid_category.id)
+
+      must_respond_with :success
+    end
+
+    it 'responds with a not_found when id given does not exist' do
+      get category_path("500")
+
+      must_respond_with :redirect
+    end
+  end
+
+
+
 end
