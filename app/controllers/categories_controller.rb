d87be854a3b772673cbf0354a12b18dd25cd1a04
Yasmin <yasmin312002@hotmail.com>
2019-10-23 15:56:45 -0700

    added tests to the categories controller and methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,49 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @categories  = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params) 
+    if @category.save 
+      flash[:success] = "Category added successfully"
+      redirect_to marchant_path 
+      return
+    else 
+      flash.now[:failure] = "Category failed to save"
+      render :new, status: :bad_request 
+      return
+    end
+  end
   
+  def edit 
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to root_path 
+      return
+    else 
+      render :edit, status: :bad_request 
+      return
+    end
+  end
+  
+  def destroy
+    @category.destroy
+    
+    redirect_to categories_path
+    return
+  end
+  
+  private
+  
+  def category_params
+    return params.require(:category).permit(:name)
+  end
 end
+  
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,38 +1,32 @@
 require "test_helper"
 
 describe CategoriesController do
-  describe "index action" do
+  describe 'index action' do
 
-    it "gives back a successful response" do
+    it 'gives back a successful response' do
       get categories_path
       must_respond_with :success
     end
 
-    it "gives back a 404 if there are no works available" do
+    it 'gives back a 404 if there are no categories available' do
       get categories_path
     
     end
   end
 
-  describe 'show action' do
-    it 'responds with a success when id given exists' do
-      valid_work = Category.first
-      
-      get category_path(valid_category.id)
-
-      must_respond_with :success
-
+  describe 'new' do
+    it 'new' do
     end
   end
 
-   describe 'show action' do
-
+  describe 'show action' do
     it 'responds with a success when id given exists' do
       valid_category = Category.first
       
       get category_path(valid_category.id)
 
       must_respond_with :success
+
     end
 
     it 'responds with a not_found when id given does not exist' do
@@ -41,7 +35,4 @@ describe CategoriesController do
       must_respond_with :redirect
     end
   end
-
-
-
 end
