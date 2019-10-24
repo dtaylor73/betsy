a160005243f77dd24e5aea911ec09d5d1fd4a09a
Rinostar <codingrinostar@gmail.com>
2019-10-24 15:03:38 -0700

    merchant actions 1/2

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,20 @@
 class MerchantsController < ApplicationController
+  def current
+    @merchant = Merchant.find(session[:user_id])
+    if @merchant.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    merchant = Merchant.find_by(uid: auth_hash[:uid], provider: params[:provider])
+    if merchant
+      flash[:success] = "Logged in as returning merchant #{merchant.name}"
+    else
+      merchant = Merchant
+    end
+  end
 end
