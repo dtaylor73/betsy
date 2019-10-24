affb4bda9a0ff1dae3ebf275223a022a9d41959d
Rinostar <codingrinostar@gmail.com>
2019-10-23 18:55:52 -0700

    fix products schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_233920) do
+ActiveRecord::Schema.define(version: 2019_10_24_014759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 2019_10_23_233920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
+    t.string "description"
+    t.string "photo_url"
+    t.boolean "status"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
