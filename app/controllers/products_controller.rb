ed701e7d5928a021da5e25b71974a8a2518268ba
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-25 21:48:14 -0700

    Added additional logic in Products controller's index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,16 +2,16 @@ class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    # if params[:merchant_id]
-    #   merchant = Merchant.find_by(id: params[:merchant_id])
-    #   @products = merchant.products
-    # elsif params[:category_id]
-    #   category = Category.find_by(id: params[:category_id])
-    #   @products = category.products
-    # else
-    #   @products = Product.all
-    # end
-    @products = Product.where(status: true)
+    if params[:merchant_id]
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      @products = merchant.products
+    elsif params[:category_id]
+      category = Category.find_by(id: params[:category_id])
+      @products = category.products
+    else
+      @products = Product.where(status: true)
+    end
+    # @products = Product.where(status: true)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   resources :products
 
+  resources :categories, only: [:new, :create] do
+    resources :products, only: [:index]
+  end
+
   patch "/products/:id/active", to: "products#toggle_active", as: "toggle_active"
 
   patch "/drivers/:id/inactive", to: "products#toggle_inactive", as: "toggle_inactive"
