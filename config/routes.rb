c190cee3bc20b57ab96870abd60a6b36b8ba5087
Rinostar <codingrinostar@gmail.com>
2019-10-27 14:54:08 -0700

    add merchant dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
   post "/logout", to: "merchants#destroy", as: "logout"
   get "/merchants/current", to: "merchants#current", as: "current_merchant"
-
+  
   patch "/products/:id/active", to: "products#toggle_active", as: "toggle_active"
   patch "/drivers/:id/inactive", to: "products#toggle_inactive", as: "toggle_inactive"
 end
