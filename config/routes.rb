e5778211f6cbba170f19f4dcefe77cb9b49ebd75
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-28 13:04:33 -0700

    Updating products show view to incorporate reviews route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
     resources :products, only: [:index]
   end
 
+  # nested reviews under products
+  resources :products do
+    resources :reviews, only: [:new, :create]
+  end
+
   patch "/products/:id/active", to: "products#toggle_active", as: "toggle_active"
 
   patch "/products/:id/inactive", to: "products#toggle_inactive", as: "toggle_inactive"
