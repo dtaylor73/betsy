abd488b53984fac496538fb3bc81bccdb5ab3fd7
Rinostar <codingrinostar@gmail.com>
2019-10-28 21:19:05 -0700

    update login & logout button in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :orders
   
   resources :merchants do
-    resources :products, only: [:index, :new]
+    resources :products, only: [:index, :new, :create]
     resources :orders, only: [:index]
   end
 
