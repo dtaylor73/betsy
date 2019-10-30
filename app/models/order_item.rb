95fd0709b30c62753e7c181c743285dbd4bcb2e3
Rinostar <codingrinostar@gmail.com>
2019-10-29 22:46:06 -0700

    update order_item model relationship

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
-  belongs_to :merchants
   has_one :merchant, through: :product
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0}
