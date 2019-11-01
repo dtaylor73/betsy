b0626e066142c4b1bf7afcd0a13d0e04070df8b2
Nicky Choi <nickyjinchoi@gmail.com>
2019-11-01 10:44:10 -0700

    Uncommented migration files

--- db/migrate/20191024014759_readd_three_columns_to_products.rb
+++ db/migrate/20191024014759_readd_three_columns_to_products.rb
@@ -1,7 +1,7 @@
 class ReaddThreeColumnsToProducts < ActiveRecord::Migration[5.2]
-  def change
-    # add_column :products, :description, :string
-    # add_column :products, :photo_url, :string
-    # add_column :products, :status, :boolean
-  end
+  # def change
+  #   # add_column :products, :description, :string
+  #   # add_column :products, :photo_url, :string
+  #   # add_column :products, :status, :boolean
+  # end
 end
