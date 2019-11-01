66d6a6e56a337ed24b60413edf160db63edb317f
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-31 21:31:51 -0700

    Commented out migration files

--- db/migrate/20191028171551_add_merchant_id_to_order_items.rb
+++ db/migrate/20191028171551_add_merchant_id_to_order_items.rb
@@ -1,6 +1,6 @@
 class AddMerchantIdToOrderItems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :order_items, :merchants, foreign_key: :true
-    change_column_default :products, :photo_url, "https://i.imgur.com/CSul8N3.jpg"
+    # add_reference :order_items, :merchants, foreign_key: :true
+    # change_column_default :products, :photo_url, "https://i.imgur.com/CSul8N3.jpg"
   end
 end
