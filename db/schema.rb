28e700a423c0cd94168c3d64deb6ede9d69a9b6b
Nicky Choi <nickyjinchoi@gmail.com>
2019-10-31 19:05:16 -0700

    Added empty migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_233330) do
+ActiveRecord::Schema.define(version: 2019_11_01_020433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,6 @@ ActiveRecord::Schema.define(version: 2019_10_29_233330) do
     t.bigint "product_id"
     t.bigint "order_id"
     t.boolean "shipping_status"
-    t.bigint "merchants_id"
-    t.index ["merchants_id"], name: "index_order_items_on_merchants_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
@@ -87,5 +85,4 @@ ActiveRecord::Schema.define(version: 2019_10_29_233330) do
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
-  add_foreign_key "order_items", "merchants", column: "merchants_id"
 end
