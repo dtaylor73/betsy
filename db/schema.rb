a6bb05e6ed3ba9a2c17cf858d4402e926d6ba676
Rinostar <codingrinostar@gmail.com>
2019-10-31 19:07:07 -0700

    fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_233330) do
+ActiveRecord::Schema.define(version: 2019_10_30_060340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_233330) do
     t.datetime "updated_at", null: false
     t.bigint "product_id"
     t.bigint "order_id"
-    t.boolean "shipping_status"
+    t.boolean "shipping_status", default: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
